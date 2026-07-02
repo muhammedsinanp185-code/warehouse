5308655425aa6ffded4d8ed3f30c5bff9294733e
Mhdsinan <muhammedsinanp185@gmail.com>
2026-07-02 17:37:54 +0530

    style: fix StockFlow logo and add profile dropdown

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -12,8 +12,8 @@
         <!-- SIDEBAR -->
         <aside class="sidebar">
             <div class="sidebar-header">
-                <div class="user-icon-circle" style="width: 40px; height: 40px; margin-bottom: 0;">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <div style="display: flex; align-items: center; justify-content: center; width: 40px; height: 40px; color: var(--text-color);">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" style="width: 32px; height: 32px;">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                     </svg>
                 </div>
